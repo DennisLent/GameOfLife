55fcf65f6574bd36fb897a855af26d13a29dc716
Dennis <d.lentschig@student.tudelft.nl>
2023-11-09 16:13:31 +0100

    cmake

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "engine.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
+void usage(){
+    printf("Usage: \n"
+            "-h: show help \n"
+            "\n"
+            "Arguments for selection: \n"
+            "-d D: Dimension of the field (DxD) \n"
+            "-t T: Type of pattern you want to use \n");
+    exit(0);
+}
+
+int main(int argc, char *argv[]){
+    int dimension;
+    char type;
+
+    int opt;
+    while((opt = getopt(argc, argv, "h:d:t")) != -1){
+        switch (opt) {
+            case 'd':
+                dimension = atoi(optarg);
+                break;
+            case 't':
+                type = *optarg;
+            case 'h':
+                usage();
+        }
+    }
+
+    return 0;
+}
--- src/engine.c
+++ src/engine.c
@@ -7,15 +7,15 @@ typedef struct {
     int height;
 } Grid;
 
-Grid initializeGrid(int width, int height){
+Grid initializeGrid(int dimension){
     Grid grid;
-    grid.height = height;
-    grid.width = width;
+    grid.height = dimension;
+    grid.width = dimension;
 
-    grid.cells = (int**)malloc(width * sizeof(int*));
-    for(int i=0; i<width; i++){
-        grid.cells[i] = (int*)malloc(height * sizeof(int*));
-        for(int j=0; j<height; j++){
+    grid.cells = (int**)malloc(grid.width * sizeof(int*));
+    for(int i=0; i<grid.width; i++){
+        grid.cells[i] = (int*)malloc(grid.height * sizeof(int*));
+        for(int j=0; j<grid.height; j++){
             grid.cells[i][j] = 0;
         }
     }
